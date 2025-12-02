b78baef44c35d372afb6ddb9310eca53d09d2783
julia <caralhodasilva04@gmail.com>
2025-12-01 21:07:59 -0300

    adiciona a classe GerarDados com métodos para gerar arrays de dados em ordem crescente, decrescente e aleatória

---
+++ GerarDados.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class GerarDados {
+    public int[] gerarCrescente(int tamanho) {
+        int[] dados = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            dados[i] = i + 1;
+        }
+        return dados;
+    }
+    public int[] gerarDecrescente(int tamanho) {
+        int[] dados = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            dados[i] = tamanho - i;
+        }
+        return dados;
+    }
+
+    public int[] gerarAleatorio(int tamanho) {
+        Random random = new Random();
+        int[] dados = new int[tamanho];
+
+        for (int i = 0; i < tamanho; i++) {
+            dados[i] = random.nextInt(1000);
+        }
+
+        return dados;
+    }
+}
